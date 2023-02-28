1a6d5da8e20c1af13b5f866976c9620d50843777
Angel Batista <angel.batista1992@gmail.com>
2023-02-28 00:53:28 -0700

    basic express set up

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || "8080";
+
+app.get("/", (req, res) => {
+  res.send("Alive");
+});
+
+app.listen(port, () => {
+  console.log(`[Relayer]: Running at http://localhost:${port}`);
+});
