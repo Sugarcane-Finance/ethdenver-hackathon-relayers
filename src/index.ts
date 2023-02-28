031348b1644c18619046cd6e4751500f5c6e4363
Angel Batista <angel.batista1992@gmail.com>
2023-02-28 00:59:15 -0700

    basic middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const port = process.env.PORT || "8080";
 
 const app = express();
-const port = process.env.PORT || "8080";
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "5mb" }));
 
 app.get("/", (req, res) => {
   res.send("Alive");
+});
+
+app.post("/", (req, res) => {
+  res.send(req.body.blah);
 });
 
 app.listen(port, () => {
